c21758954b9f6c86edfeaab80d226cb115c2799d
webflyx <zavala180395@gmail.com>
2020-10-13 16:46:18 +0300

    create btn "getUsers"

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,14 +6,16 @@ import userPhoto from "../../../src/assets/images/user.png"
 
 let Users = (props) => {
 
-    if(props.users.length === 0){
+    let getUsers = () => 
+        {
+            if(props.users.length === 0){
 
-        axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => { 
-            props.setUsers(response.data.items)
-        })
-
+                    axios
+                    .get("https://social-network.samuraijs.com/api/1.0/users")
+                    .then(response => { 
+                        props.setUsers(response.data.items)
+                    })
+                }
         // props.setUsers( [
         //     { id: 1, photoUrl: "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png", followed: false, fullName: "Sergei", status: "I'm create site", location: { city: "Kiev", country: "Ukraine" } },
         //     { id: 2, photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxUAaxOy8jnQYmYxXZOjcrq_iFW_Zjlbvv-A&usqp=CAU", followed: true, fullName: "Dima", status: "HELLO PEOPLE", location: { city: "Moscow", country: "Russia" } },
@@ -21,10 +23,12 @@ let Users = (props) => {
         //     { id: 4, photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRreMcZBkJH8HScfuswuiCkxCvTu7Kf85z-rg&usqp=CAU", followed: false, fullName: "Andrei", status: "Let's go!", location: { city: "Kiev", country: "Ukraine" } },
         // ]
         //   )
-    }
+            }
 
   return(
+
     <div>
+              <button onClick={getUsers}>Get users</button>
         {
             props.users.map( u => <div key={u.id}>
                 <span>

a15217c7f747310a90ddd3437d7a8d42937a912b
webflyx <zavala180395@gmail.com>
2020-10-13 11:29:52 +0300

    API get users (51)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import style from "./Users.module.css"
+import style from "./Users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../../src/assets/images/user.png"
 
 
 let Users = (props) => {
 
     if(props.users.length === 0){
-        props.setUsers( [
-            { id: 1, photoUrl: "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png", followed: false, fullName: "Sergei", status: "I'm create site", location: { city: "Kiev", country: "Ukraine" } },
-            { id: 2, photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxUAaxOy8jnQYmYxXZOjcrq_iFW_Zjlbvv-A&usqp=CAU", followed: true, fullName: "Dima", status: "HELLO PEOPLE", location: { city: "Moscow", country: "Russia" } },
-            { id: 3, photoUrl: "https://icon-library.com/images/4_avatar-512.png", followed: true, fullName: "Nastya", status: "I am so pretty", location: { city: "Minsk", country: "Belarus" } },
-            { id: 4, photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRreMcZBkJH8HScfuswuiCkxCvTu7Kf85z-rg&usqp=CAU", followed: false, fullName: "Andrei", status: "Let's go!", location: { city: "Kiev", country: "Ukraine" } },
-        ]
-          )
+
+        axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => { 
+            props.setUsers(response.data.items)
+        })
+
+        // props.setUsers( [
+        //     { id: 1, photoUrl: "https://icons-for-free.com/iconfiles/png/512/business+face+people+icon-1320086457520622872.png", followed: false, fullName: "Sergei", status: "I'm create site", location: { city: "Kiev", country: "Ukraine" } },
+        //     { id: 2, photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxUAaxOy8jnQYmYxXZOjcrq_iFW_Zjlbvv-A&usqp=CAU", followed: true, fullName: "Dima", status: "HELLO PEOPLE", location: { city: "Moscow", country: "Russia" } },
+        //     { id: 3, photoUrl: "https://icon-library.com/images/4_avatar-512.png", followed: true, fullName: "Nastya", status: "I am so pretty", location: { city: "Minsk", country: "Belarus" } },
+        //     { id: 4, photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRreMcZBkJH8HScfuswuiCkxCvTu7Kf85z-rg&usqp=CAU", followed: false, fullName: "Andrei", status: "Let's go!", location: { city: "Kiev", country: "Ukraine" } },
+        // ]
+        //   )
     }
 
   return(
@@ -20,7 +29,7 @@ let Users = (props) => {
             props.users.map( u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={style.userPhoto}/>
+                        <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={style.userPhoto}/>
                     </div>
                     <div>
                         {u.followed ? <button onClick={ () => {props.unfollow(u.id)}}>Follow</button> : <button onClick={ () => {props.follow(u.id)}}>Unfollow</button>}
@@ -29,12 +38,12 @@ let Users = (props) => {
                 </span>
                 <span>
                 <span>
-                    <div>{u.fullName}</div>
-                    <div>{u.userStatus}</div>
+                    <div>{u.name}</div>
+                    <div>{u.status}</div>
                 </span>
                 <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
                 </span>
                 </span>
             </div> )
